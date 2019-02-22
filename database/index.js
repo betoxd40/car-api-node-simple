94cac88de126976830402dcec86f0de0e4f8738e
Alberto Yanes <albertoyanesjr3@hotmail.com>
2019-02-22 10:24:53 -0300

    Added mongodb cluster

--- database/index.js
+++ database/index.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
+// const uri = "mongodb+srv://kay:myRealPassword@cluster0.mongodb.net/admin";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//  // perform actions on the collection object
+//   client.close();
+// });
+
 const dbConfig = config.get('dbConfig');
 
 // Connection to Mongoose
